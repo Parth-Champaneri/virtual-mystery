7ff2767b7e9f270610dcbf7e7ea44c6bad490718
parthchampaneri <parthchampaneri@mail.utoronto.ca>
2018-07-31 00:59:54 -0400

    commit check

---
+++ src/vm-angular/src/app/artifact-view/artifact-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artifact-view',
+  templateUrl: './artifact-view.component.html',
+  styleUrls: ['./artifact-view.component.css']
+})
+export class ArtifactViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
